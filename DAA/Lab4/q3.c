a77ab55a922360e0fbdd1e42c60ca4ebbb1fc109
Aritra Pal <aritra147pal@gmail.com>
2023-09-02 01:05:07 +0530

    Updated DAA Lab4

--- DAA/Lab4/q3.c
+++ DAA/Lab4/q3.c
@@ -1,25 +1,19 @@
 /*
     Given 2 integers x and n. Write a program to design your own
-    power function to compute x^n with time complexity O9logn).
+    power function to compute x^n with time complexity O(logn).
 */
 
 #include <stdio.h>
 
 int power(int x, int n) {
-    int res = 1;
-    if (x == 1 || n == 1)
-        return x;
-    else if (n == 0)
-        return 1;
-    else {
-        while (n > 1) {
-            if (n % 2 == 0)
-                res *= x * x;
-            else 
-                res *= x * x * x;
-            n /= 2;
-        }
-        return res;
+    if (n == 0) 
+        return 1.0;
+    else if (n % 2 == 0) {
+        double temp = power(x, n / 2);
+        return temp * temp;
+    } else {
+        double temp = power(x, (n - 1) / 2);
+        return x * temp * temp;
     }
 }
 
@@ -30,6 +24,6 @@ int main() {
     printf("Enter power: ");
     scanf("%d", &n);
 
-    printf("%d^%d = %d\n\n", x, n, power(x, n));
+    printf("\n%d^%d = %d\n\n", x, n, power(x, n));
     return 0;
 }
